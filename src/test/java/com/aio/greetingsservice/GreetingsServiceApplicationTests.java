ee83700b8385183472e67e48ca1b248b57358c67
gent.sami@gmail.com <github.13>
2020-02-22 19:19:43 +0330

    Initial commit

---
+++ src/test/java/com/aio/greetingsservice/GreetingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aio.greetingsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
